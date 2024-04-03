bccf534dd9e9e482b266a96b19469960f966b3c8
Chakradhar Cherala <68188570+chakricherala@users.noreply.github.com>
2024-04-03 16:54:03 +0530

    added animations

--- script.js
+++ script.js
@@ -1,40 +1,84 @@
-gsap.registerPlugin(ScrollTrigger);
-
+var timeout;
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".smooth-scroll"),
   smooth: true
 });
-locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy(".smooth-scroll", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+// let circle = document.querySelector("#circle")
+
+// window.addEventListener("mousemove", function(dets){
+//     circle.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+// })
+
+// let hover = document.querySelectorAll(".sec")
+
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#circle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#circle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleChaptaKaro();
+circleMouseFollower();
+// firstPageAnim();
+
+// teeno element ko sleect karo, uske baad teeno par ek mousemove lagao, jab mousemove ho to ye pata karo ki mouse kaha par hai, jiska matlab hai mouse ki x and y position pata karo, ab mouse ki x y position ke badle us image ko show karo and us image ko move karo, move karte waqt rotate karo, and jaise jaise mouse tez chale waise waise rotation bhi tez ho jaye
+
+document.querySelectorAll(".sec").forEach(function (sec) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  sec.addEventListener("mouseleave", function (dets) {
+    gsap.to(sec.querySelector(".sec img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  sec.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - sec.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(sec.querySelector(".sec img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
 });
-
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-ScrollTrigger.refresh();
-
-let circle = document.querySelector("#circle")
-
-window.addEventListener("mousemove", function(dets){
-    // console.log(dets.clientX, dets.clientY)
-    circle.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
-    // gsap.to("circle",{
-    //     x: "dets.clientX",
-    //     y: "dets.clientY",
-    //     duration: 2,
-    //     ease: Expo
-    // })
-})
-
-
 
 
 // let tl = gsap.timeline()
